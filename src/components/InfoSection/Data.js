f2ff378222a2306d6d3dc541a6c922b1b61e0b5b
LucasbarthTI <lucasbarth@ienh.com.br>
2022-04-12 16:37:51 -0300

    v5

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -7,6 +7,7 @@ export const homeObjOne = {
     headline: 'Unlimited Transactions with zero fees',
     description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
     buttonLabel: 'Get started',
+    imgStart: false,
     img: require('../../images/svg-1.svg'),
     alt: 'Car',
     dark: true,
